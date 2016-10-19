fe3e37ae6d301a5f0640224a3f5fac14f89d7ca4
normi-fikri <fikri@normi.co.id>
2016-10-19 11:29:46 +0700

    Add google verifiy, analytics, and meta descriptions

--- applications/resources/views/front/career.blade.php
+++ applications/resources/views/front/career.blade.php
@@ -5,8 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Toby's Estate Indonesia</title>
+  <meta name="google-site-verification" content="Nh4YlU54424bnwr7LXglUypRD8uldfvXa5btnnAgs9I" />
   <meta name="description" content="Welcome to the Toby's Estate Indonesia where you will find everything you need to know about our coffee, tea, chocolate and more!">
-  <meta name="author" content="tobysestatecoffee.co.id">
+  <meta name="keywords" content="Toby's Estate Indonesia, coffee, tobys, estate, coffee indonesia, tobysestate, Tobys Estate Indonesia, 'barisata', 'career'"/>
+  <meta http-equiv="Content-Language" content="id" />
+  <meta name="robots" content="index, follow" />
+  <meta name="googlebot" content="all" />
 
   <link rel="shortcut icon" type="image/png" href="./img/icon.png"/>
 
@@ -15,7 +19,16 @@
   <link href="./fonts/fonts.css" rel="stylesheet">
   <link href="./css/styles.css" rel="stylesheet">
 
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-85998402-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
   <!-- Custom CSS -->
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- applications/resources/views/front/index.blade.php
+++ applications/resources/views/front/index.blade.php
@@ -5,7 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Toby's Estate Indonesia</title>
+    <meta name="google-site-verification" content="Nh4YlU54424bnwr7LXglUypRD8uldfvXa5btnnAgs9I" />
     <meta name="description" content="Welcome to the Toby's Estate Indonesia where you will find everything you need to know about our coffee, tea, chocolate and more!">
+    <meta name="keywords" content="Toby's Estate Indonesia, coffee, tobys, estate, coffee indonesia, tobysestate, Tobys Estate Indonesia"/>
+    <meta http-equiv="Content-Language" content="id" />
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot" content="all" />
+
     <meta name="author" content="tobysestatecoffee.co.id">
 
     <link rel="shortcut icon" type="image/png" href="./img/icon.png"/>
@@ -19,6 +25,16 @@
     <script>alert('{{ Session::get('message')}}');</script>
     @endif
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-85998402-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     <!-- Custom CSS -->
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
